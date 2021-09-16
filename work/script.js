aad5f15d9cb05a79d1878d6d9e5d671685b6affd
Andrii <melnikandrij070@gmil.com>
2021-09-16 23:18:55 +0300

    Im complited second task!

--- work/script.js
+++ work/script.js
@@ -1,5 +1,5 @@
 "use strict";
-/* First program
+
 
 const numderOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
 
@@ -11,18 +11,38 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt("Один из просмотреных фильмов?", ""),
-      b = prompt(" ОЦЕНКА?",),
-      c = prompt("Один из просмотреных фильмов?", ""),
-      d = prompt(" ОЦЕНКА?");
-
-      personalMovieDB.movies[a] = b;
-      personalMovieDB.movies[c] = d;
 
       console.log(personalMovieDB);
-      
-*/
+      for ( let i = 0; i < 2; i++){
+        const a = prompt("Один из просмотреных фильмов?", ""),
+            b = prompt(" ОЦЕНКА?",);
+        if ( a != null && b != null && a != "" && b != "" && a.length < 50){
+            personalMovieDB.movies[a] = b;
+            console.log("Mission complite");
+        
+        }
+        else{
+            console.log("err");
+            i--;
+        }
 
+        if ( personalMovieDB.count < 10) {
+            console.log("Ви переглянули дуже мало фільмів");
+        }else if (10 < personalMovieDB.count < 30){
+            console.log("ви переглянули достатню кількість фільмів");
+        }else if (personalMovieDB.count >= 50){
+            console.log("Ви гуу перегляду фільмів");
+        }else{
+            console.log(",'''''''''''''''''''");
+        }
+
+        
+        
+      }
+      
+
+
+/*
 const num = 50;
 ( num == 50 ) ? console.log("OK!") : console.log("eRRoR");
 
@@ -33,4 +53,31 @@ switch (num) {
     case 100:
         console.log("NOO!");
         break;
+    case 50:
+        console.log("YEEES!");
+        break;
+    default:
+        console.log("Not today Brooo!");
+        break;
 };
+
+
+let num = 0;
+
+while ( num < 55 ){
+    console.log(num);
+    num++;
+}
+
+do {
+    console.log(num);
+    num++;
+}
+ while( num < 55 );
+
+ for (let i = 0; i < 10; i+=2){
+    if (i == 6){
+        continue;
+    } 
+    console.log(i)
+ }*/
