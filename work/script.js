26c2729a7f18891cb59225d6685fde52b9adce9c
Andrii <melnikandrij070@gmil.com>
2021-09-19 14:46:00 +0300

    Continue work with Arr and Obj

--- work/script.js
+++ work/script.js
@@ -1,8 +1,112 @@
  "use strict";
 
+
+let a = 5,
+    b = a;
+
+
+b = b + 5;
+console.log(b);
+console.log(a);
+
+
+const obj = {
+    a: 5,
+    b: 1
+}
+
+console.log(obj);
+
+const copy = obj;
+copy.a = 10;
+
+console.log(obj);
+console.log(copy);
+
+function copyArr (mainObj){
+let copyObj = {};
+
+    for (let key in mainObj){
+        copyObj[key] = mainObj[key];
+    }
+    return copyObj;
+}
+
+const numbers = {
+    a: 5,
+    b: 444, 
+    c: {
+        x: 5,
+        y: 55
+    } 
+};
+
+const copyNumbers = copyArr(numbers);
+
+console.log(numbers);
+console.log(copyNumbers);
+
+
+const add = {
+    d: 66, 
+    e: 44
+};
+
+console.log(Object.assign(numbers, add));
+
+const clone = Object.assign({}, add);
+
+clone.d = 3333;
+
+console.log(clone, add);
+
+const  oldArr = ['10', 'dfffff', 'ffffffffssssss', '  '];
+const newArr = oldArr.slice();
+
+newArr[0] = "ddddd";
+
+console.log(oldArr, newArr);
+
+
+const video = ['youtube', 'tiktok', 'megogo'],
+      blogs = ['bloggers', 'lifejournals'],
+      internet = [...video, ...blogs,'facebook', 'vk'];
+
+console.log(internet);
+
+function log (a, b, c){
+    console.log(a, b, c);
+}
+
+const num = [1, 2, 555];
+
+log(...num);
+
+const q = {
+    one: 1,
+    two: 2
+};
+
+const newQ = {...q};
+
+console.log(newQ);
+
+
+
+
+
+
+
+
+
+
+
+
+
+ /*
  const arr = [1,8,3,4,5];
 
- arr.sort(compareNum);
+arr.sort(compareNum);
 function compareNum(a, b){
     return a - b;
 }
@@ -32,7 +136,7 @@ arr.forEach(function(item, i, arr) {
  const products = str.split(", ");
  products.sort();
  console.log(products.join("; "));
-
+*/
 
 
 
@@ -94,106 +198,106 @@ console.log(Object.keys(options).length);
 
 
 
-/*
+ /*
 
-function first(){
-    //Do something
+ function first(){
+     //Do something
 
-    setTimeout(function(){
-        console.log(1);
-    }, 500);
-}
+     setTimeout(function(){
+         console.log(1);
+     }, 500);
+ }
 
-function second(){
-    console.log(2);
-}
+ function second(){
+     console.log(2);
+ }
 
-first();
-second();
+ first();
+ second();
 
 
-function learnJS(lang, callback){
-    console.log(`я учу: ${lang}`);
-    callback();
-}
+ function learnJS(lang, callback){
+     console.log(`я учу: ${lang}`);
+     callback();
+ }
 
-learnJS("JS", function(){
-    console.log("я прошол этот урок");
-});
-*/
-/*
-let numberOfFilms;
+ learnJS("JS", function(){
+     console.log("я прошол этот урок");
+ });
+ */
+ /*
+ let numberOfFilms;
 
-function start() {
-    numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+ function start() {
+     numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
 
-    while (numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms == "") {
-        numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
-    }
-}
+     while (numberOfFilms == null || isNaN(numberOfFilms) || numberOfFilms == "") {
+         numberOfFilms = +prompt("Сколько фильмов вы уже посмотрели?", "");
+     }
+ }
 
-start();
+ start();
 
-const personalMovieDB = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+ const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false
+ };
 
 
 
 
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt("Один из просмотреных фильмов?", ""),
-            b = prompt(" ОЦЕНКА?", );
-        if (a != null && b != null && a != "" && b != "" && a.length < 50) {
-            personalMovieDB.movies[a] = b;
-            console.log("Mission complite");
+ function rememberMyFilms() {
+     for (let i = 0; i < 2; i++) {
+         const a = prompt("Один из просмотреных фильмов?", ""),
+             b = prompt(" ОЦЕНКА?", );
+         if (a != null && b != null && a != "" && b != "" && a.length < 50) {
+             personalMovieDB.movies[a] = b;
+             console.log("Mission complite");
 
-        } else {
-            console.log("err");
-            i--;
-        }
-    }
+         } else {
+             console.log("err");
+             i--;
+         }
+     }
 
-    rememberMyFilms();
+     rememberMyFilms();
 
 
-    function detectPersonalLevel() {
-        if (personalMovieDB.count < 10) {
-            console.log("Ви переглянули дуже мало фільмів");
-        } else if (10 < personalMovieDB.count < 30) {
-            console.log("ви переглянули достатню кількість фільмів");
-        } else if (personalMovieDB.count >= 50) {
-            console.log("Ви гуу перегляду фільмів");
-        } else {
-            console.log(",'''''''''''''''''''");
-        }
-    }
-    detectPersonalLevel();
+     function detectPersonalLevel() {
+         if (personalMovieDB.count < 10) {
+             console.log("Ви переглянули дуже мало фільмів");
+         } else if (10 < personalMovieDB.count < 30) {
+             console.log("ви переглянули достатню кількість фільмів");
+         } else if (personalMovieDB.count >= 50) {
+             console.log("Ви гуу перегляду фільмів");
+         } else {
+             console.log(",'''''''''''''''''''");
+         }
+     }
+     detectPersonalLevel();
 
 
-}
+ }
 
-function showMyDB(hidden) {
-    if (!hidden) {
-        console.log(personalMovieDB);
-    }
-}
+ function showMyDB(hidden) {
+     if (!hidden) {
+         console.log(personalMovieDB);
+     }
+ }
 
-showMyDB(personalMovieDB.privat);
+ showMyDB(personalMovieDB.privat);
 
-function wrireYourGeners() {
-    for (let i = 1; i <= 3; i++) {
-        personalMovieDB.genres[i - 1] = prompt(`Напишите Ваш любимый жанр под номером: ${i}`);
-    }
-}
+ function wrireYourGeners() {
+     for (let i = 1; i <= 3; i++) {
+         personalMovieDB.genres[i - 1] = prompt(`Напишите Ваш любимый жанр под номером: ${i}`);
+     }
+ }
 
-wrireYourGeners();
-*/
+ wrireYourGeners();
+ */
 
 
 
@@ -208,155 +312,155 @@ wrireYourGeners();
 
 
 
-/*
-let num = 20;
+ /*
+ let num = 20;
 
 
-function showFirstMessage(text){
-    console.log(text);
-     num = 15;
-}
+ function showFirstMessage(text){
+     console.log(text);
+      num = 15;
+ }
 
 
 
-console.log(num); 
+ console.log(num); 
 
 
-showFirstMessage("Hello world!");
+ showFirstMessage("Hello world!");
 
-console.log(num); 
+ console.log(num); 
 
 
-function calc(a, b){
-    return (a + b);
-}
+ function calc(a, b){
+     return (a + b);
+ }
 
-console.log(calc(5, 88));
+ console.log(calc(5, 88));
 
-function ret(){
-    let num = 50;
-    return num;
-}
+ function ret(){
+     let num = 50;
+     return num;
+ }
 
-const anothreNum = ret();
-console.log(anothreNum);
+ const anothreNum = ret();
+ console.log(anothreNum);
 
-const logger  = function() {
-    console.log("fccfcfcfc");
-};
+ const logger  = function() {
+     console.log("fccfcfcfc");
+ };
 
-logger();
+ logger();
 
-const calc = (a, b) => {return a + b};
+ const calc = (a, b) => {return a + b};
 
-*/
+ */
 
-/*
-const str = "test";
-console.log(str.toUpperCase());
-console.log(str.toLowerCase());
-console.log(str);
+ /*
+ const str = "test";
+ console.log(str.toUpperCase());
+ console.log(str.toLowerCase());
+ console.log(str);
 
-const fruit = "Some fruit";
+ const fruit = "Some fruit";
 
-console.log(fruit.indexOf("7"));
+ console.log(fruit.indexOf("7"));
 
-const logg = " Hello World";
+ const logg = " Hello World";
 
-console.log(logg.slice(7, 9));
+ console.log(logg.slice(7, 9));
 
-console.log(logg.substring(7, 9));
+ console.log(logg.substring(7, 9));
 
-console.log(logg.substr(7, 5));
+ console.log(logg.substr(7, 5));
 
-const num = 12.9;
+ const num = 12.9;
 
-console.log(Math.round(num));
+ console.log(Math.round(num));
 
-const test = "12.2px";
-console.log(parseFloat(test));
-    */
+ const test = "12.2px";
+ console.log(parseFloat(test));
+     */
 
 
-/*
+ /*
 
-const numderOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
+ const numderOfFilms = +prompt("Сколько фильмов Вы посмотрели?", "");
 
-const personalMovieDB = {
-    count: numderOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
+ const personalMovieDB = {
+     count: numderOfFilms,
+     movies: {},
+     actors: {},
+     genres: [],
+     privat: false
+ };
 
 
-      console.log(personalMovieDB);
-      for ( let i = 0; i < 2; i++){
-        const a = prompt("Один из просмотреных фильмов?", ""),
-            b = prompt(" ОЦЕНКА?",);
-        if ( a != null && b != null && a != "" && b != "" && a.length < 50){
-            personalMovieDB.movies[a] = b;
-            console.log("Mission complite");
-        
-        }
-        else{
-            console.log("err");
-            i--;
-        }
+       console.log(personalMovieDB);
+       for ( let i = 0; i < 2; i++){
+         const a = prompt("Один из просмотреных фильмов?", ""),
+             b = prompt(" ОЦЕНКА?",);
+         if ( a != null && b != null && a != "" && b != "" && a.length < 50){
+             personalMovieDB.movies[a] = b;
+             console.log("Mission complite");
+         
+         }
+         else{
+             console.log("err");
+             i--;
+         }
 
-        if ( personalMovieDB.count < 10) {
-            console.log("Ви переглянули дуже мало фільмів");
-        }else if (10 < personalMovieDB.count < 30){
-            console.log("ви переглянули достатню кількість фільмів");
-        }else if (personalMovieDB.count >= 50){
-            console.log("Ви гуу перегляду фільмів");
-        }else{
-            console.log(",'''''''''''''''''''");
-        }
+         if ( personalMovieDB.count < 10) {
+             console.log("Ви переглянули дуже мало фільмів");
+         }else if (10 < personalMovieDB.count < 30){
+             console.log("ви переглянули достатню кількість фільмів");
+         }else if (personalMovieDB.count >= 50){
+             console.log("Ви гуу перегляду фільмів");
+         }else{
+             console.log(",'''''''''''''''''''");
+         }
 
-        
-        
-      }
-      */
+         
+         
+       }
+       */
 
 
-/*
-const num = 50;
-( num == 50 ) ? console.log("OK!") : console.log("eRRoR");
+ /*
+ const num = 50;
+ ( num == 50 ) ? console.log("OK!") : console.log("eRRoR");
 
-switch (num) {
-    case 49: 
-        console.log("NOO!");
-        break;
-    case 100:
-        console.log("NOO!");
-        break;
-    case 50:
-        console.log("YEEES!");
-        break;
-    default:
-        console.log("Not today Brooo!");
-        break;
-};
+ switch (num) {
+     case 49: 
+         console.log("NOO!");
+         break;
+     case 100:
+         console.log("NOO!");
+         break;
+     case 50:
+         console.log("YEEES!");
+         break;
+     default:
+         console.log("Not today Brooo!");
+         break;
+ };
 
 
-let num = 0;
+ let num = 0;
 
-while ( num < 55 ){
-    console.log(num);
-    num++;
-}
+ while ( num < 55 ){
+     console.log(num);
+     num++;
+ }
 
-do {
-    console.log(num);
-    num++;
-}
- while( num < 55 );
+ do {
+     console.log(num);
+     num++;
+ }
+  while( num < 55 );
 
- for (let i = 0; i < 10; i+=2){
-    if (i == 6){
-        continue;
-    } 
-    console.log(i)
- }*/
+  for (let i = 0; i < 10; i+=2){
+     if (i == 6){
+         continue;
+     } 
+     console.log(i)
+  }*/
